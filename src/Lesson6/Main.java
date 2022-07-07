61a25c5af9a3bbb4ad5a9e005a870857cae175c9
Surfer <krasir@mail.ru>
2022-07-07 23:55:45 +0800

    Done HW Lesson 6

---
+++ src/Lesson6/Main.java
@@ -0,0 +1,30 @@
+package Lesson6;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args)  throws IOException {
+        HttpURLConnection httpURLConnection = (HttpURLConnection) new URL("https://api.weather.yandex.ru/v2/forecast?lat=59&lon=30&limit=5&hours=false").openConnection();
+        httpURLConnection.setRequestProperty("X-Yandex-API-Key", "a1f43bb2-52af-4ee9-bd55-75b8b96a2eeb");
+        System.out.println(httpURLConnection.getResponseCode());
+        System.out.println(httpURLConnection.getRequestMethod());
+        System.out.println(httpURLConnection.getResponseMessage());
+        Map<String, List<String>> headers = httpURLConnection.getHeaderFields();
+        for (String k: headers.keySet()) {
+            System.out.printf("Ключ: %s, значение: %s \n", k, headers.get(k));
+        }
+
+
+        try (InputStream inputStream = httpURLConnection.getInputStream();){
+            int c;
+            while ((c = inputStream.read())!=-1){
+                System.out.print((char) c);
+            }
+        }
+    }
+}
